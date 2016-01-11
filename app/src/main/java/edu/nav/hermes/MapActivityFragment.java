af47972e16a3226c79ad66ab9172db53474fdfb1
eduardo sobral <easobral@gmail.com>
2016-01-11 21:57:39 -0200

    added myLocationOverlay

--- app/src/main/java/edu/nav/hermes/MapActivityFragment.java
+++ app/src/main/java/edu/nav/hermes/MapActivityFragment.java
@@ -1,17 +1,27 @@
 package edu.nav.hermes;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MapActivityFragment extends Fragment {
+
+
+    private MyLocationNewOverlay mLocationOverlay;
+    private CompassOverlay mCompassOverlay;
 
     public MapActivityFragment() {
     }
@@ -23,6 +33,39 @@ public class MapActivityFragment extends Fragment {
         MapView mapView = (MapView) rootView.findViewById(R.id.map);
         mapView.setBuiltInZoomControls(true);
         mapView.setMultiTouchControls(true);
+
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(9);
+        GeoPoint startPoint = new GeoPoint(-22.905727, -43.289553);
+        mapController.setCenter(startPoint);
+
+        this.mLocationOverlay = new MyLocationNewOverlay(getContext(), new GpsMyLocationProvider(getContext()), mapView);
+        mapView.getOverlays().add(this.mLocationOverlay);
+
         return rootView;
+    }
+
+
+    /**
+     * Called when the fragment is visible to the user and actively running.
+     * This is generally
+     * tied to {@link Activity#onResume() Activity.onResume} of the containing
+     * Activity's lifecycle.
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.mLocationOverlay.enableMyLocation();
+    }
+
+    /**
+     * Called when the Fragment is no longer resumed.  This is generally
+     * tied to {@link Activity#onPause() Activity.onPause} of the containing
+     * Activity's lifecycle.
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.mLocationOverlay.disableMyLocation();
     }
 }
